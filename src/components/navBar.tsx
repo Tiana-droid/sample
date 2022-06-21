545930125ff2dfada9e6b618df8ac18778e047e2
Saviee <saviourise1@gmail.com>
2022-06-21 15:16:19 +0100

    nav bar functionality

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect, useState } from "react";
 import "../styles/navBar.css";
 import { BiUser, BiSearchAlt, BiCart } from "react-icons/bi";
 
@@ -13,6 +14,43 @@ interface Props {
 }
 
 const NavBar = (props: Props) => {
+  const [showDiv, setshowDiv] = useState("none");
+
+  //   const shopRef = useRef<HTMLDivElement | null>(null);
+  //   const accountRef = useRef<HTMLDivElement | null>(null);
+  //   const categoryRef = useRef<HTMLDivElement | null>(null);
+  //   const subscriptionRef = useRef<HTMLDivElement | null>(null);
+  const shop = useRef<HTMLLIElement | null>(null);
+  const account = useRef<HTMLLIElement | null>(null);
+  const category = useRef<HTMLLIElement | null>(null);
+  const subscription = useRef<HTMLLIElement | null>(null);
+  useEffect(() => {
+    shop.current?.addEventListener("mouseover", () => {
+      setshowDiv("shop");
+    });
+    shop.current?.addEventListener("mouseout", () => {
+      setshowDiv("none");
+    });
+    account.current?.addEventListener("mouseover", () => {
+      setshowDiv("account");
+    });
+    account.current?.addEventListener("mouseout", () => {
+      setshowDiv("account");
+    });
+    category.current?.addEventListener("mouseover", () => {
+      setshowDiv("category");
+    });
+    category.current?.addEventListener("mouseout", () => {
+      setshowDiv("none");
+    });
+    subscription.current?.addEventListener("mouseover", () => {
+      setshowDiv("subscription");
+    });
+    subscription.current?.addEventListener("mouseout", () => {
+      setshowDiv("none");
+    });
+  }, []);
+
   return (
     <div className="navBarContainer">
       <section className="topNav">
@@ -36,16 +74,60 @@ const NavBar = (props: Props) => {
       <section className="bottomNav">
         <ul>
           <li className={props.page === "home" ? "active" : "li"}>Home</li>
-          <li className={props.page === "shop" ? "active" : "li"}>Shop</li>
-          <li className={props.page === "account" ? "active" : "li"}>
-            Account
+          <li className={props.page === "shop" ? "active" : "li"} ref={shop}>
+            Shop
+            <div className={showDiv === "shop" ? "showDiv" : "hideDiv"}>
+              <ol>
+                <li>Restaurant</li>
+                <li>Grocery</li>
+              </ol>
+            </div>
           </li>
-          <li className={props.page === "category" ? "active" : "li"}>
+          <li
+            className={props.page === "account" ? "active" : "li"}
+            ref={account}
+          >
+            Account
+            <div className={showDiv === "account" ? "showDiv" : "hideDiv"}>
+              <ol>
+                <li>Sign Up As A Buyer</li>
+                <li>Sign Up As A Merchant</li>
+                <li>Sign In</li>
+                <li>My Account</li>
+              </ol>
+            </div>
+          </li>
+          <li
+            className={props.page === "category" ? "active" : "li"}
+            ref={category}
+          >
             Categories
+            <div className={showDiv === "category" ? "showDiv" : "hideDiv"}>
+              <ol>
+                <li>Cereal Grain</li>
+                <li>Cooking Oil</li>
+                <li>Seasoning And Spices</li>
+                <li>Vegetables And Fruits</li>
+                <li>Protein (Fish $ Meat)</li>
+                <li>Tubers</li>
+                <li>Floor</li>
+                <li>Canned Food</li>
+                <li>Snacks</li>
+                <li>Cooked Food</li>
+              </ol>
+            </div>
           </li>
           <li className={props.page === "about" ? "active" : "li"}>About Us</li>
-          <li className={props.page === "subscription" ? "active" : "li"}>
+          <li
+            className={props.page === "subscription" ? "active" : "li"}
+            ref={subscription}
+          >
             Subscription
+            <div className={showDiv === "subscription" ? "showDiv" : "hideDiv"}>
+              <ol>
+                <li>Monthly subsciption</li>
+              </ol>
+            </div>
           </li>
           <li className={props.page === "contact" ? "active" : "li"}>
             Contact Us
