f148b342fc8fadd2e0b7d46a1102b4356ede9ac0
Adeolu David <adeolusegun99@gmail.com>
2023-01-05 03:16:49 +0100

    added trnasition to slider

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react"
 import "../styles/howWeOperate.scss"
-
+import ReactCSSTransitionGroup from "react-transition-group"
 import slide1 from "../assets/stepsSlide1.png"
 import slide2 from "../assets/slide3.png"
 import slide3 from "../assets/slider3.png"
+import { CSSTransition } from "react-transition-group"
 
 function ImageSlider() {
   const [index, setIndex] = useState(0)
@@ -19,8 +20,15 @@ function ImageSlider() {
   }, [index, interval, images.length])
 
   return (
-    <div className="carousel">
-      <img src={images[index]} alt="slider-img" />
+    <div className="slideshow">
+      <CSSTransition
+        key={index}
+        in={index === index}
+        timeout={5000}
+        classNames="slide"
+      >
+        <img src={images[index]} alt="slider-img" />
+      </CSSTransition>
     </div>
   )
 }
